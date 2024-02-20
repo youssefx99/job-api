71d64f6866d61d3a627e65509730db110207d14e
youssefx99 <yousseffaisal20@gmail.com>
2024-02-20 10:45:47 +0200

    first

---
+++ app.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+require("express-async-errors");
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+// routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  },
+  console.log("connected successfully to DataBase")
+);
+
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
+// routes
+app.use("/api/v1/users", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`listen on port ${port}`));
